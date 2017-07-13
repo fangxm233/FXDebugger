b3b4516ccb23b7c2f0264f2f4029829347975085
fangxm <1126716733@qq.com>
2017-07-13 21:23:37 +0800

    fangxm

--- Source/Tester/Program.cs
+++ Source/Tester/Program.cs
@@ -13,9 +13,6 @@ namespace Tester
             new Debug().Initialization();
             Debug.Debugger.Pause();
             Debug.Debugger.Log("fangxm 6666666,Asixa 更666666");
-<<<<<<< HEAD
-=======
->>>>>>> 9d28e5b544115dfe73da0e5708c2b45a5de86f9e
            
 
             Debug.Debugger.Log("是是是");
