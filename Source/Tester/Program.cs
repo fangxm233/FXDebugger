b3c84780689d89b6a164544da3624d170eb7752f
asixa <asixa@foxmail.com>
2017-07-13 21:16:50 +0800

    asixa

--- Source/Tester/Program.cs
+++ Source/Tester/Program.cs
@@ -13,12 +13,9 @@ namespace Tester
             new Debug().Initialization();
             Debug.Debugger.Pause();
             Debug.Debugger.Log("fangxm 6666666,Asixa 更666666");
-<<<<<<< HEAD
             //Asixa
-=======
             Debug.Debugger.Log("是是是");
             //test//fangxm
->>>>>>> cf9def9e3621873b2351cd0c5a1fe3062426eb18
             Debug.Debugger.Pause();
             Debug.Debugger.Close();
         }
